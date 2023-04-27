0b2d65033fa08c2a700400216e97039beeb39cd5
hieuct <hieuct@huongvietgroup.com>
2023-04-27 07:53:41 +0700

    commit

--- CoreMVC/Models/Dao/UserDAO.cs
+++ CoreMVC/Models/Dao/UserDAO.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Data;
+using System.Data.SqlClient;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,8 +15,16 @@ namespace Models.Dao
         StoreDbContext db = new StoreDbContext();
         public byte Login(string username, string password)
         {
-            //d_Login_Info item=db.FirstOrDefault()
-            return 0;
+            d_Login_Info item = db.d_Login_Info.FirstOrDefault(x => x.Username == username && x.Password == password);
+            if(item == null)
+			{
+                //0: username hoặc password không đúng
+                return 0;
+            }
+			else
+			{
+                return (byte)item.Status;
+			}
         }
     }
 }
